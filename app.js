e4e84c33e74b8acc18077f75fa83dc2ea314333a
lnx1337 <lnx1337@customer-187-174-213-84.uninet-ide.com.mx>
2013-09-07 19:29:54 -0500

    cambios

--- app.js
+++ app.js
@@ -167,11 +167,11 @@ console.log(req.files);
                               
 
 
-                              db.insert('tbl_fotos', { url: picture , longitud: longitudeDecimal , latitud: latitudeDecimal }, function(err,foto){
+                              db.insert('tbl_fotos', { url: picture ,latitud: latitudeDecimal, longitud: longitudeDecimal }, function(err,foto){
  
-                                   console.log(foto);
+
                                       
-                                  /*
+                                  
                                        db.insert(' tbl_fotos_has_tbl_panic_alerts', { tbl_fotos_id:foto.insertId, alert_id:alert.insertId,abonado_id:cabonado_id}, function(err,foto){
 
                     
@@ -190,7 +190,7 @@ console.log(req.files);
                                                      });
                                                                                                      
                                              });
-                                   */
+                                   
 
    
                               });
