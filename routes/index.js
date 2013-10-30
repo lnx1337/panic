f92de902468dfe7fffb2448050f8ccbd4bec43a9
Jair <dev.lnx1337@gmail.com>
2013-10-30 13:05:10 -0600

    cambios

--- routes/index.js
+++ routes/index.js
@@ -145,7 +145,7 @@ exports.trakingGPS=function (req,res){
       var clongitude=req.body.longitude;
 
       db.insert('tbl_tracking_gps', { alert_id: calertid, abonado_id:cabonado_id , alert_type_id:calert_type_id, latitude:clatitude, longitude:clongitude, dat_time: getDateTime() }, function(err, info){ 
-             res.send('{ alert_id:"'+info.insertId+'",abonado_id:"'+cabonado_id+'",alert_type_id:"'+calert_type_id+'"}');  
+             res.send('{ id:"'+info.insertId+'",alert_id:"'+calertid+'",abonado_id:"'+cabonado_id+'",alert_type_id:"'+calert_type_id+'"}');  
       });
  
    
